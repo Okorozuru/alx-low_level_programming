aa1fda56bb5cce67d51d5566d8702812f17e8853
Emmanuel <okorozuruemmanuel@gmailcom>
2023-03-23 05:18:43 -0700

    debugging_task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,14 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
+
 /**
-* main - causes an infinite loop
-* Return: 0
-*/
+ * main - Prints a random number and states whether
+ *it is positive, negative, or zero.
+ * Return: Always 0.
+ */
+
 int main(void)
 {
-int i;
-	i  = 0;
-	postive_or_negative;
+	int n;
 
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
 	return (0);
-
 }
