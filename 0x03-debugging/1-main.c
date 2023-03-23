86960ace4ae1848bf86ffed7348bbddd4198593d
Emmanuel <okorozuruemmanuel@gmailcom>
2023-03-23 04:36:51 -0700

    debugging_task

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -3,7 +3,6 @@
 * main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
 {
 int i;
@@ -13,7 +12,6 @@ i = 0;
 /*{*/
 /*putchar(i);*/
 /*}*/
-
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
