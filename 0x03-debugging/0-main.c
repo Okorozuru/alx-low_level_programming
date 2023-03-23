a5dd9cff12cd7be35367a0a0c5c71e6b5a5f05b0
Emmanuel <okorozuruemmanuel@gmailcom>
2023-03-23 05:15:41 -0700

    debugging_task

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,19 +1,14 @@
-#include "main.h"
-
+#include <stdio.h>
 /**
-* main - Test function for positive or negative
-*
+* main - causes an infinite loop
 * Return: 0
 */
-
 int main(void)
-
 {
 int i;
-i = 0;
+	i  = 0;
+	postive_or_negative;
 
-positive_or_negative(i);
-
-return (0);
+	return (0);
 
 }
