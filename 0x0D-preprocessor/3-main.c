fa43c377702c97db019d64d084766732a922cec5
Emmanuel <okorozuruemmanuel@gmail.com>
2023-04-24 04:35:47 -0700

    3-function_like_marco.h

---
+++ 0x0D-preprocessor/3-main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "3-function_like_macro.h"
+#include "3-function_like_macro.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	x = ABS(-98) * 10;
+	y = ABS(98) * 10;
+printf("%d, %d\n", x, y);
+return (0);
+}
